ecfff8eac5d851cd1af2fc2048b0b1e7a5a63898
Tulsi-Pal <tulsi.22b0131109@abes.ac.in>
2026-05-03 15:50:24 +0530

    Initial commit - frontend and backend

---
+++ ems-backend/src/main/java/net/tulsi/ems_backend/mapper/EmployeeMapper.java
@@ -0,0 +1,23 @@
+package net.tulsi.ems_backend.mapper;
+
+import net.tulsi.ems_backend.dto.EmployeeDto;
+import net.tulsi.ems_backend.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
